ff7ac8615fda571c5776878fd103f6d4f3012ff1
trongson <trongson18101999@gmail.com>
2024-04-18 11:00:24 +0700

    feat: add feature for group

--- src/route/features/group/group.resolver.ts
+++ src/route/features/group/group.resolver.ts
@@ -10,9 +10,6 @@ import {
 import { GroupService } from './group.service';
 import { UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/route/auth/guard/jwt-auth.guard';
-import { RolesGuard } from 'src/route/auth/guard/roles.guard';
-import { Roles } from 'src/route/auth/decorators/roles.decorator';
-import { EnumRole } from 'src/ts/enum';
 import { PaginationGroupDto } from './dto/common.dto';
 import { ParamsQueryDto, ResponseDto } from 'src/ts/common';
 import { GroupDto } from './dto/group.dto';
@@ -20,6 +17,7 @@ import { CreateGroupDto } from './dto/create-group.dto';
 import { UpdateGroupDto } from './dto/update-group.dto';
 import { Group } from './enitites/group.entity';
 import { UserService } from '../user/user.service';
+import { getUserIdFromJwt } from 'src/helper/getIdUserFromJwt';
 
 @Resolver(() => GroupDto)
 export class GroupResolver {
@@ -46,20 +44,55 @@ export class GroupResolver {
   @UseGuards(JwtAuthGuard)
   @Mutation(() => GroupDto)
   createGroup(@Args('body') body: CreateGroupDto, @Context() context) {
-    const userId = context.req.user.userId.replace('secret-key', '');
-    return this.groupService.createGroup({ ...body, author: userId });
+    const userId = getUserIdFromJwt(context);
+    return this.groupService.createGroup(body, userId);
   }
 
   @UseGuards(JwtAuthGuard)
   @Mutation(() => ResponseDto)
-  updateGroup(@Args('id') id: string, @Args('body') body: UpdateGroupDto) {
-    return this.groupService.updateGroup(id, body);
+  updateGroup(
+    @Args('id') id: string,
+    @Args('body') body: UpdateGroupDto,
+    @Context() context,
+  ) {
+    const userId = getUserIdFromJwt(context);
+    return this.groupService.updateGroup(id, userId, body);
   }
 
   @UseGuards(JwtAuthGuard)
   @Mutation(() => ResponseDto)
-  deleteGroup(@Args('id') id: string) {
-    return this.groupService.deleteGroup(id);
+  deleteGroup(@Args('id') id: string, @Context() context) {
+    const userId = getUserIdFromJwt(context);
+    return this.groupService.deleteGroup(id, userId);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Mutation(() => ResponseDto)
+  joinGroup(@Args('id') id: string, @Context() context) {
+    const userId = getUserIdFromJwt(context);
+    return this.groupService.joinGroup(id, userId);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Mutation(() => ResponseDto)
+  acceptMemberReqJoinGroup(
+    @Args('idGroup') idGroup: string,
+    @Args('idMemberReq') idMemberReq: string,
+    @Context() context,
+  ) {
+    const userId = getUserIdFromJwt(context);
+    return this.groupService.acceptMemberReqJoinGroup(
+      idGroup,
+      userId,
+      idMemberReq,
+    );
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Mutation(() => ResponseDto)
+  leaveGroup(@Args('id') id: string, @Context() context) {
+    const userId = getUserIdFromJwt(context);
+    return this.groupService.leaveGroup(id, userId);
   }
 
   @ResolveField()
--- src/route/features/group/group.service.ts
+++ src/route/features/group/group.service.ts
@@ -47,11 +47,13 @@ export class GroupService {
     }
   }
 
-  async createGroup(body: CreateGroupDto): Promise<Group> {
+  async createGroup(body: CreateGroupDto, idAuthor: string): Promise<Group> {
     try {
       return this.groupRepository.save({
         id: uuid(),
         ...body,
+        author: idAuthor,
+        admins: [idAuthor],
       });
     } catch (error) {
       throw new BadRequestException('Error creating group');
@@ -60,6 +62,7 @@ export class GroupService {
 
   async updateGroup(
     id: string,
+    idUser: string,
     body: UpdateGroupDto,
   ): Promise<{ status: number; message: string }> {
     try {
@@ -68,6 +71,13 @@ export class GroupService {
       if (!group) {
         throw new NotFoundException('Group not found');
       }
+
+      const isAdmin = group.admins.includes(idUser);
+
+      if (!isAdmin) {
+        throw new BadRequestException('You are not admin of this group');
+      }
+
       group = { ...group, ...body };
       await this.groupRepository.save(group);
 
@@ -80,12 +90,20 @@ export class GroupService {
     }
   }
 
-  async deleteGroup(id: string): Promise<{ status: number; message: string }> {
+  async deleteGroup(
+    id: string,
+    idUser: string,
+  ): Promise<{ status: number; message: string }> {
     try {
       const group = await this.groupRepository.findOne({ where: { id } });
 
       if (!group) {
         throw new NotFoundException('Group not found');
+      }
+
+      const isAdmin = group.admins.includes(idUser);
+      if (!isAdmin) {
+        throw new BadRequestException('You are not admin of this group');
       }
       await this.groupRepository.remove(group);
 
@@ -95,6 +113,76 @@ export class GroupService {
       };
     } catch (error) {
       throw new BadRequestException(`Error deleting group`);
+    }
+  }
+
+  async joinGroup(
+    idGroup: string,
+    idUser: string,
+  ): Promise<{ status: number; message: string }> {
+    try {
+      const group = await this.groupRepository.findOne({
+        where: { id: idGroup },
+      });
+      if (!group) {
+        throw new NotFoundException('Group not found');
+      }
+      group.membersReq = [idUser, ...group.membersReq];
+      await this.groupRepository.save(group);
+      return {
+        status: HttpStatus.OK,
+        message: 'Join group successfully',
+      };
+    } catch (error) {
+      throw new BadRequestException('Error joining group');
+    }
+  }
+
+  async acceptMemberReqJoinGroup(
+    idGroup: string,
+    idUser: string,
+    idMemberReq: string,
+  ): Promise<{ status: number; message: string }> {
+    try {
+      const group = await this.groupRepository.findOne({
+        where: { id: idGroup },
+      });
+      if (!group) {
+        throw new NotFoundException('Group not found');
+      }
+      group.members = [idUser, ...group.members];
+      group.membersReq = group.membersReq.filter(
+        (i: string) => i !== idMemberReq,
+      );
+      await this.groupRepository.save(group);
+      return {
+        status: HttpStatus.OK,
+        message: 'Accept member req join group successfully',
+      };
+    } catch (error) {
+      throw new BadRequestException('Error accept member req join group');
+    }
+  }
+
+  async leaveGroup(
+    idGroup: string,
+    idUser: string,
+  ): Promise<{ status: number; message: string }> {
+    try {
+      const group = await this.groupRepository.findOne({
+        where: { id: idGroup },
+      });
+      if (!group) {
+        throw new NotFoundException('Group not found');
+      }
+      group.members = group.members.filter((i: string) => i !== idUser);
+      await this.groupRepository.save(group);
+      return {
+        status: HttpStatus.OK,
+        message: 'Leave group successfully',
+      };
+    } catch (error) {
+      throw new BadRequestException('Error leaving group');
     }
   }
 }
